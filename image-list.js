ed5ddf332f077a46e28e2fb7fcd4d91a81a52cc6
Maxim <maxkravch97@yandex.ru>
2018-03-16 12:23:38 +0100

    fixed all errors

--- image-list.js
+++ image-list.js
@@ -1,26 +1,26 @@
-var Path = 'gallery/';
+var Path = 'gallery/'
 
-var Gallery = { 'images' : [
-      
+var Gallery = { 'images': [
+
   {
-    'name'  : 'Darth Vader',
-    'alt' : 'A Black Clad warrior lego toy',
+    'name': 'Darth Vader',
+    'alt': 'A Black Clad warrior lego toy',
     'url': 'gallery/myLittleVader.jpg',
     'credit': '<a href="https://www.flickr.com/photos/legofenris/">legOfenris</a>, published under a <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">Attribution-NonCommercial-NoDerivs 2.0 Generic</a> license.'
   },
 
   {
-    'name'  : 'Snow Troopers',
-    'alt' : 'Two lego solders in white outfits walking across an icy plain',
+    'name': 'Snow Troopers',
+    'alt': 'Two lego solders in white outfits walking across an icy plain',
     'url': 'gallery/snowTroopers.jpg',
     'credit': '<a href="https://www.flickr.com/photos/legofenris/">legOfenris</a>, published under a <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">Attribution-NonCommercial-NoDerivs 2.0 Generic</a> license.'
   },
 
   {
-    'name'  : 'Bounty Hunters',
-    'alt' : 'A group of bounty hunters meeting, aliens and humans in costumes.',
+    'name': 'Bounty Hunters',
+    'alt': 'A group of bounty hunters meeting, aliens and humans in costumes.',
     'url': 'gallery/bountyHunters.jpg',
     'credit': '<a href="https://www.flickr.com/photos/legofenris/">legOfenris</a>, published under a <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">Attribution-NonCommercial-NoDerivs 2.0 Generic</a> license.'
-  },
-  
-]};
+  }
+
+]}
--- sw.js
+++ sw.js
@@ -1,41 +1,43 @@
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
+  console.log('first step')
   event.waitUntil(
-    caches.open('v1').then(function(cache) {
+    caches.open('v1').then(function (cache) {
+      console.log('opened cache')
       return cache.addAll([
-        '/sw-test/',
-        '/sw-test/index.html',
-        '/sw-test/style.css',
-        '/sw-test/app.js',
-        '/sw-test/image-list.js',
-        '/sw-test/star-wars-logo.jpg',
-        '/sw-test/gallery/bountyHunters.jpg',
-        '/sw-test/gallery/myLittleVader.jpg',
-        '/sw-test/gallery/snowTroopers.jpg'
-      ]);
-    })
-  );
-});
+        'index.html',
+        'style.css',
+        'app.js',
+        'image-list.js',
+        'star-wars-logo.jpg',
+        'gallery/bountyHunters.jpg',
+        'gallery/myLittleVader.jpg',
+        'gallery/snowTroopers.jpg'
+      ])
+    }).then(console.log('done with cache'))
+  )
+})
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(caches.match(event.request).then(function(response) {
+self.addEventListener('fetch', function (event) {
+  console.log('started fetch')
+  event.respondWith(caches.match(event.request).then(function (response) {
     // caches.match() always resolves
     // but in case of success response will have value
     if (response !== undefined) {
-      return response;
+      return response
     } else {
       return fetch(event.request).then(function (response) {
         // response may be used only once
         // we need to save clone to put one copy in cache
         // and serve second one
-        let responseClone = response.clone();
-        
+        let responseClone = response.clone()
+
         caches.open('v1').then(function (cache) {
-          cache.put(event.request, responseClone);
-        });
-        return response;
+          cache.put(event.request, responseClone)
+        })
+        return response
       }).catch(function () {
-        return caches.match('/sw-test/gallery/myLittleVader.jpg');
-      });
+        return caches.match('gallery/myLittleVader.jpg')
+      })
     }
-  }));
-});
+  }))
+})

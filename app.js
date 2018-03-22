6d67fb89dde0ae505d4ebdf9bb0a8da3c89c77b8
Maxim <maxkravch97@yandex.ru>
2018-03-22 20:20:39 +0100

    push notifications now work

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ if ('serviceWorker' in navigator) {
     } else if (reg.active) {
       console.log('Service worker active')
     }
+
+    swRegistration = reg
+    initializeUI()
   }).catch(function (error) {
     // registration failed
     console.log('Registration failed with ' + error)
@@ -53,4 +56,119 @@ window.onload = function () {
       console.log(Error)
     })
   }
+}
+
+const applicationServerPublicKey = 'BKuzy_SL2P2ugC4uGvBuo_F-NZKTLk-C3rQfkuZW6pjcsaSTJqY9G8yLPWZgcPDVLO8FJGkJXnonpqAlLVU8k9A'
+
+const pushButton = document.querySelector('#subscription')
+
+let isSubscribed = false
+let swRegistration = null
+
+function urlB64ToUint8Array (base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4)
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/')
+
+  const rawData = window.atob(base64)
+  const outputArray = new Uint8Array(rawData.length)
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i)
+  }
+  return outputArray
+}
+
+function updateBtn () {
+  if (Notification.permission === 'denied') {
+    pushButton.textContent = 'Push Messaging Blocked.'
+    pushButton.disabled = true
+    updateSubscriptionOnServer(null)
+    return
+  }
+
+  if (isSubscribed) {
+    pushButton.textContent = 'Disable Push Messaging'
+  } else {
+    pushButton.textContent = 'Enable Push Messaging'
+  }
+
+  pushButton.disabled = false
+}
+
+function updateSubscriptionOnServer (subscription) {
+  // TODO: Send subscription to application server
+
+  if (subscription) {
+    console.log(JSON.stringify(subscription))
+  }
+}
+
+function subscribeUser () {
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey)
+  swRegistration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: applicationServerKey
+  })
+  .then(function (subscription) {
+    console.log('User is subscribed.')
+
+    updateSubscriptionOnServer(subscription)
+
+    isSubscribed = true
+
+    updateBtn()
+  })
+  .catch(function (err) {
+    console.log('Failed to subscribe the user: ', err)
+    updateBtn()
+  })
+}
+
+function unsubscribeUser () {
+  swRegistration.pushManager.getSubscription()
+  .then(function (subscription) {
+    if (subscription) {
+      return subscription.unsubscribe()
+    }
+  })
+  .catch(function (error) {
+    console.log('Error unsubscribing', error)
+  })
+  .then(function () {
+    updateSubscriptionOnServer(null)
+
+    console.log('User is unsubscribed.')
+    isSubscribed = false
+
+    updateBtn()
+  })
+}
+
+function initializeUI () {
+  pushButton.addEventListener('click', function () {
+    pushButton.disabled = true
+    if (isSubscribed) {
+      unsubscribeUser()
+    } else {
+      subscribeUser()
+    }
+  })
+
+  // Set the initial subscription value
+  swRegistration.pushManager.getSubscription()
+  .then(function (subscription) {
+    isSubscribed = !(subscription === null)
+
+    updateSubscriptionOnServer(subscription)
+
+    if (isSubscribed) {
+      console.log('User IS subscribed.')
+    } else {
+      console.log('User is NOT subscribed.')
+    }
+
+    updateBtn()
+  })
 }
--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ self.addEventListener('install', function (event) {
         'index.html',
         'style.css',
         'app.js',
-        // 'sw.js',
+        'sw.js',
         'image-list.js',
         'star-wars-logo.jpg',
         'gallery/bountyHunters.jpg',
@@ -22,5 +22,64 @@ self.addEventListener('fetch', function (event) {
   caches.match(event.request).then(function (response) {
     return response || fetch(event.request)
   })
+  )
+})
+
+const applicationServerPublicKey = 'BKuzy_SL2P2ugC4uGvBuo_F-NZKTLk-C3rQfkuZW6pjcsaSTJqY9G8yLPWZgcPDVLO8FJGkJXnonpqAlLVU8k9A'
+
+/* eslint-enable max-len */
+
+function urlB64ToUint8Array (base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4)
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/')
+
+  const rawData = window.atob(base64)
+  const outputArray = new Uint8Array(rawData.length)
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i)
+  }
+  return outputArray
+}
+
+self.addEventListener('push', function (event) {
+  console.log('[Service Worker] Push Received.')
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`)
+  let text = `${event.data.text()}`
+  console.log(text)
+  const title = 'Push Codelab'
+  const options = {
+    body: text,
+    icon: 'images/icon.png',
+    badge: 'images/badge.png'
+  }
+
+  event.waitUntil(self.registration.showNotification(title, options))
+})
+
+self.addEventListener('notificationclick', function (event) {
+  console.log('[Service Worker] Notification click Received.')
+
+  event.notification.close()
+
+  event.waitUntil(
+    clients.openWindow('https://developers.google.com/web/')
+  )
+})
+
+self.addEventListener('pushsubscriptionchange', function (event) {
+  console.log('[Service Worker]: \'pushsubscriptionchange\' event fired.')
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey)
+  event.waitUntil(
+    self.registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: applicationServerKey
+    })
+    .then(function (newSubscription) {
+      // TODO: Send to application server
+      console.log('[Service Worker] New subscription: ', newSubscription)
+    })
   )
 })
